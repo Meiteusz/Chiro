771343f0e67fd298a5db4b22f77e59274f7bd7fa
Matheus Teixeira <matheustexeirablu@hotmail.com>
2024-10-30 22:27:43 -0300

    rollback

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -3,14 +3,6 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:encryptedProjectBoardId',
-        destination: '/[encryptedProjectBoardId]',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
